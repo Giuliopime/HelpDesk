50fec36e7944e0e0b71b3efc73994b80d3c1f8b7
giuliopime <giuliopime@gmail.com>
2020-08-29 15:07:42 +0200

    Created embed commands

--- commands/Embed/setImage.js
+++ commands/Embed/setImage.js
@@ -11,6 +11,15 @@ module.exports = {
     // Command Category
     embed: true,
     async execute(data, member, message, args, index) {
-
+        let url = args[0];
+        if(args[0] === '{delete}') url = undefined;
+        const isValid = message.client.isValidURL(url);
+        if(!isValid && url !== '{delete}') {
+            message.client.errorEmbed.setDescription('You need to provide a correct URL for an image.\nYou can use tools like [imgbb](https://it.imgbb.com/) to get a permanent URL for an image.\n\nAlternatively you can use `{delete}` to remove the image from the embed.');
+            return message.channel.send(message.client.errorEmbed);
+        }
+        await message.client.guildSchema.updateOne({guildID: message.guild.id},  { $set:{ ['helpDesks.' + index + '.embedProperties.image.url']: url } });
+        message.client.replyEmbed.setDescription('Image set.\nUse `hd?update` to apply the changes to the #help-desk embed.');
+        await message.channel.send(message.client.replyEmbed);
     },
 };
--- commands/Embed/setURL.js
+++ commands/Embed/setURL.js
@@ -1,10 +1,10 @@
 module.exports = {
     // Info
-    name: 'setauthorurl',
-    description: 'Set an image to for the #help-desk embed',
-    aliases: ['sauthorurl'],
+    name: 'seturl',
+    description: 'Set an url for the title of the #help-desk embed',
+    aliases: ['surl'],
     args: true,
-    usage: '<imageURL> or <{delete}> to remove',
+    usage: '<URL> or <{delete}> to remove',
     cooldown: 5,
     // Basic checks
     guildOnly: true,
@@ -18,8 +18,8 @@ module.exports = {
             message.client.errorEmbed.setDescription('You need to provide a correct URL.\nAlternatively you can use `{delete}` to remove the image from the embed.');
             return message.channel.send(message.client.errorEmbed);
         }
-        await message.client.guildSchema.updateOne({guildID: message.guild.id},  { $set:{ ['helpDesks.' + index + '.embedProperties.author.url']: url } });
-        message.client.replyEmbed.setDescription('Author URL set.\nUse `hd?update` to apply the changes to the #help-desk embed.');
+        await message.client.guildSchema.updateOne({guildID: message.guild.id},  { $set:{ ['helpDesks.' + index + '.embedProperties.url']: url } });
+        message.client.replyEmbed.setDescription('Title URL set.\nUse `hd?update` to apply the changes to the #help-desk embed.');
         await message.channel.send(message.client.replyEmbed);
     },
 };
