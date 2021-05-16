c7a5211370b4ae3caab60c7665ff630a76e9c379
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2021-05-16 16:27:30 +0200

    Questions, answers and reactions managment

---
+++ src/main/kotlin/dev/giuliopime/helpdesk/bot/commands/help_desk/sub/edit_sub/NotiChannel.kt
@@ -0,0 +1,4 @@
+package dev.giuliopime.helpdesk.bot.commands.help_desk.sub.edit_sub
+
+class NotiChannel {
+}
--- src/main/kotlin/dev/giuliopime/helpdesk/bot/internals/extentions/TextChannelExtentions.kt
+++ src/main/kotlin/dev/giuliopime/helpdesk/bot/internals/extentions/TextChannelExtentions.kt
@@ -29,3 +29,63 @@ suspend inline fun TextChannel.awaitMessageOrNull(
         return@withTimeoutOrNull event.message
     }
 }
+
+suspend inline fun TextChannel.awaitSpecificMessage(
+    userID: String,
+    okStrings: List<String> = listOf(),
+    timeout: Long = 60000,
+    autoDelete: Boolean = true
+): String? {
+    return withTimeoutOrNull(timeout) {
+        val event = jda.await<GuildMessageReceivedEvent> {
+            it.author.id == userID
+                    && it.channel.id == id
+                    && okStrings.any { string -> string.toLowerCase().startsWith(it.message.contentRaw.toLowerCase()) }
+        }
+
+        if (autoDelete) {
+            try { event.message.delete().queue() }
+            catch (ignored: ErrorResponseException) { }
+            catch (ignored: InsufficientPermissionException) { }
+        }
+
+        return@withTimeoutOrNull okStrings.find { it.toLowerCase().startsWith(event.message.contentRaw.toLowerCase()) }
+            ?.toLowerCase()
+    }
+}
+
+
+suspend inline fun TextChannel.awaitNumericMessage(
+    userID: String,
+    cancelString: String,
+    upperLimit: Int,
+    lowerLimit: Int = 1,
+    timeout: Long = 60000,
+    crossinline filter: (GuildMessageReceivedEvent) -> Boolean = { true },
+    autoDelete: Boolean = true
+): Int? {
+    return withTimeoutOrNull(timeout) {
+        val event = jda.await<GuildMessageReceivedEvent> {
+            filter(it)
+                    && it.author.id == userID
+                    && it.channel.id == id
+                    && (it.message.contentRaw.toLowerCase() == cancelString ||
+                    (it.message.contentRaw.toIntOrNull() != null
+                            && it.message.contentRaw.toInt() >= lowerLimit
+                            && it.message.contentRaw.toInt() <= upperLimit))
+        }
+
+        if (autoDelete) {
+            try { event.message.delete().queue() }
+            catch (ignored: ErrorResponseException) { }
+            catch (ignored: InsufficientPermissionException) { }
+        }
+
+        if (event.message.contentRaw.toLowerCase() == cancelString)
+            return@withTimeoutOrNull -1
+
+        return@withTimeoutOrNull event.message.contentRaw.toIntOrNull()
+    }
+}
+
+
